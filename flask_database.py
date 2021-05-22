41dda648d428374e1952dc1fa907dd05d1b05b8a
Jennifer Park <jenniferpark@ucsb.edu>
2021-05-22 15:41:02 -0700

    edited flask_database.py

--- flask_database.py
+++ flask_database.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -18,3 +18,30 @@ class User_profile(db.Model):
     acidity = db.Column(db.Integer, unique = False, nullable = False)
     allergies = db.Column(db.String(800), unique = False, nullable = False)
     toppings = db.Column(db.String(800), unique = False, nullable = False)
+
+class Ingredient_tuple(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    quantity =  db.Column(db.Integer, unique=False, nullable=False)
+    measurement =  db.Column(db.String(800), unique=False, nullable=True)
+    ingredient = db.Column(db.String(800), unique=False, nullable=True)
+    recipeName = db.Column(db.String(800), unique=False, nullable=True)
+
+## dict["blueberry smoothie"] = [(2,cups,yogurt),(1, tablespoon,honey), (1, bucket, feet)]
+@app.route('/', methods=['POST'])
+def postRecipe():
+    d = {}
+    d["blueberry smoothie"] = [(2,"cups","yogurt"),(1, "tablespoon","honey"),(1, "bucket", "feet")]
+    for key in d:
+        ing = Ingredient_tuple(quantity=d[key][0], measurement=d[key][1], ingredient=d[key][2], recipeName=key)
+        db.session.add(ing)
+    db.session.commit()
+    return 'Hello, Flask!'
+
+@app.route('/', methods=['GET'])
+def getRecipe():
+    query_parameters = request.args
+    recipe_name = query_parameters.get("recipe_title")
+    print(recipe_name)
+    ings = models.Ingredient_tuple.query.filter_by(recipeName=recipe_name).all()
+    print(ings)
+    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
